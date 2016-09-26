4982e70538aeea03abedd8bf34e6fd302fe46087
esther <358466847@qq.com>
2016-09-26 16:56:02 +0800

    Add blacklist import function

--- src/main/scala/cn/finance/hove/dataimport/Main.scala
+++ src/main/scala/cn/finance/hove/dataimport/Main.scala
@@ -2,6 +2,7 @@ package cn.finance.hove.dataimport
 
 import cn.finance.hove.dataimport.common.CommonDataImport
 import cn.finance.hove.dataimport.executor.{ActiveGambleAppCountExecutor, OnlineGambleUrlExecutor}
+import java.io.File
 
 object Main {
   def main(args: Array[String]): Unit = {
@@ -29,6 +30,12 @@ object Main {
         CommonDataImport.importData(filePrefix, threadCount)(provider = (tc: Int, tb: Int, fp: String) => {
           new ActiveGambleAppCountExecutor(tc, tb, fp)
         })
+      }
+      case Array("external-blacklist",file:String) => {
+        BlackNameImport.doImport("external-blacklist",new File(file))
+      }
+      case Array("dailianmeng-blacklist",file:String) => {
+        BlackNameImport.doImport("dailianmeng-blacklist",new File(file))
       }
       case _ => {
         println("Arguments is not correct...")
